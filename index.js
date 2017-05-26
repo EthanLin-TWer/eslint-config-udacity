f0d034c2620bca82dd7e9bce7f5bf3422a01f0e6
Linesh <linesh.simpcity@gmail.com>
2017-05-26 16:43:15 +0800

    [Linesh] 🎨Style: format js(although it's actually eslint config)

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jquery": true,
-    "jasmine": true
+  env: {
+    browser: true,
+    es6: true,
+    jquery: true,
+    jasmine: true,
   },
-  "plugins": [
-    "import"
+  plugins: [
+    'import',
   ],
-  "rules": {
-    "no-trailing-spaces": "error",
-    "indent": [ "error", 2 ],
-    "unicode-bom": [ "error", "never" ],
-    "no-warning-comments": "warn",
-    "semi": "error",
-    "no-extra-semi": "error",
-    "no-new-wrappers": "error",
-    "guard-for-in": "error",
-    "no-multi-str": "error",
-    "no-array-constructor": "error",
-    "no-new-object": "error",
-    "camelcase": "error",
-    "brace-style": "error",
-    "object-curly-spacing": "error",
-    "object-property-newline": "error",
-    "array-bracket-spacing": "error",
-    "quotes": [ "error", "single" ],
-    "no-undef": "error"
-  }
+  rules: {
+    'no-trailing-spaces': 'error',
+    indent: ['error', 2],
+    'unicode-bom': ['error', 'never'],
+    'no-warning-comments': 'warn',
+    semi: 'error',
+    'no-extra-semi': 'error',
+    'no-new-wrappers': 'error',
+    'guard-for-in': 'error',
+    'no-multi-str': 'error',
+    'no-array-constructor': 'error',
+    'no-new-object': 'error',
+    camelcase: 'error',
+    'brace-style': 'error',
+    'object-curly-spacing': 'error',
+    'object-property-newline': 'error',
+    'array-bracket-spacing': 'error',
+    quotes: ['error', 'single'],
+    'no-undef': 'error',
+  },
 };
