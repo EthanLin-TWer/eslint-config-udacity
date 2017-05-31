dfa10823f54a18a578a9e1a6dec1e24dc2ccf09c
Linesh <linesh.simpcity@gmail.com>
2017-05-31 12:02:05 +0800

    [Linesh] 🚨Test: this is not working for some reason, will investigate

--- test/general-meta-rules.spec.js
+++ test/general-meta-rules.spec.js
@@ -17,21 +17,22 @@ describe('General Meta Rules', () => {
     })
 
     it('should use UTF-8 with no BOM', () => {
-      const utf8WithNoBOM = iconv.encode('var foobar = 3;', 'utf-8')
+      const utf8WithNoBOM = iconv.encode('var foobar = 3;', 'utf8')
       const result = engine.executeOnText(utf8WithNoBOM)
       assert.equal(result.errorCount, 0)
     })
 
     it.skip('should not use UTF-8 with BOM', () => {
-      const utf8WithBOMs = iconv.encode('var foobar = 3;', 'utf-8', { addBOM: true })
+      const utf8WithBOMs = iconv.encode('var foobar = 3;', 'utf8', { addBOM: true })
       const result = engine.executeOnText(utf8WithBOMs)
 
       assert.equal(result.errorCount, 1)
     })
 
-    it('should not use other encodings except UTF-8', () => {
-      const result = engine.executeOnText('')
-      assert.equal(result.errorCount, 0)
+    it.skip('should not use other encodings except UTF-8', () => {
+      const asciiEncoding = iconv.encode('var foobar = 3;', 'ascii')
+      const result = engine.executeOnText(asciiEncoding)
+      assert.equal(result.errorCount, 1)
     })
   })
 
