ffc03b217bcd96dc0005299c99815674747af28b
Linesh <linesh.simpcity@gmail.com>
2017-05-31 10:21:35 +0800

    [Linesh] 🚨Test: add first test case

--- test/variables.spec.js
+++ test/variables.spec.js
@@ -1,3 +1,16 @@
+import { CLIEngine } from 'eslint'
+import { describe, it } from 'mocha'
 import assert from 'assert'
 
-assert.ok(1 === 2)
+describe('linter', () => {
+  it('first test', () => {
+    const engine = new CLIEngine({
+      useEslintrc: false,
+      rules: {
+        semi: 2,
+      },
+    })
+    const result = engine.executeOnText('var foo = 23')
+    assert.equal(result.errorCount, 0, result.results[0].messages[0].message)
+  })
+})
