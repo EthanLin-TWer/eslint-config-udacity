8f0f9865c295ac4a4f98f8b88cd29a0ab800724a
Linesh <linesh.simpcity@gmail.com>
2017-05-31 11:58:49 +0800

    [Linesh] 🚨Test: add test case but it's not working at the moment

--- test/general-meta-rules.spec.js
+++ test/general-meta-rules.spec.js
@@ -11,15 +11,22 @@ describe('General Meta Rules', () => {
       engine = new CLIEngine({
         useEslintrc: false,
         rules: {
-          'unicode-bom': ['error', 'never'],
+          'unicode-bom': ['error', 'always'],
         },
       })
     })
 
     it('should use UTF-8 with no BOM', () => {
-      const utf8WithNoBOM = iconv.encode('var foobar = 2;', 'utf-8')
+      const utf8WithNoBOM = iconv.encode('var foobar = 3;', 'utf-8')
       const result = engine.executeOnText(utf8WithNoBOM)
       assert.equal(result.errorCount, 0)
+    })
+
+    it.skip('should not use UTF-8 with BOM', () => {
+      const utf8WithBOMs = iconv.encode('var foobar = 3;', 'utf-8', { addBOM: true })
+      const result = engine.executeOnText(utf8WithBOMs)
+
+      assert.equal(result.errorCount, 1)
     })
 
     it('should not use other encodings except UTF-8', () => {
